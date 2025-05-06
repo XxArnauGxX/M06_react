498271affad4c93a0e13edcfadafad36c319fd16
Arnau Gil Gil <162440885+XxArnauGxX@users.noreply.github.com>
2025-05-06 11:41:09 +0200

    S02: ejercicio 2

---
+++ src/app/contact/page.jsx
@@ -0,0 +1,36 @@
+import "./contact.css";
+
+export default function Contact() {
+  return (
+    <>
+      <h1 className="title">Formulario de inscripción</h1>
+      <p className="description">
+        Aquí tienes un breve formulario para quienes deseen inscribirse en la
+        misión y enviar su solicitud.
+      </p>
+      <form action="#" className="form">
+        <section className="form-section">
+          <label htmlFor="name" className="label">
+            Nombre:
+          </label>
+          <input type="text" name="name" className="input" />
+        </section>
+        <section className="form-section">
+          <label htmlFor="email" className="label">
+            Correo electrónico:
+          </label>
+          <input type="email" name="email" className="input" />
+        </section>
+        <section className="form-section">
+          <label htmlFor="message" className="label">
+            Mensaje:
+          </label>
+          <input type="text" name="message" className="input input-message" />
+        </section>
+        <button type="submit" className="button">
+          Enviar
+        </button>
+      </form>
+    </>
+  );
+}
